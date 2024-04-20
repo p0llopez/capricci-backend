68b2c4fc68a313e0a3f4fb51e61ada3776af4e5c
Pol Lopez <pol.lopez.cano@gmail.com>
2024-04-20 20:16:08 +0200

    fix: python version for render

--- build.sh
+++ build.sh
@@ -11,7 +11,3 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
-
-# Start the server
-echo "Starting the server..."
-uvicorn config.wsgi:application

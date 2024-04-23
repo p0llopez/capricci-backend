dc252adca1540ecd7061e9fdc88e803dd9eb85ce
Pol Lopez <pol.lopez.cano@gmail.com>
2024-04-23 21:02:10 +0200

    feat(customer): improved customer and added to the admin

--- src/customer/admin.py
+++ src/customer/admin.py
@@ -1,1 +1,11 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Customer
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email", "phone_number")
+    search_fields = ("first_name", "last_name", "email", "phone_number")
+    list_filter = ("first_name", "last_name", "email", "phone_number")
+    ordering = ("first_name", "last_name", "email", "phone_number")
--- src/customer/models/customer.py
+++ src/customer/models/customer.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.hashers import make_password
+from django.core.validators import RegexValidator
 from django.db import models
 
 from src.commons.models import BaseModel
@@ -8,7 +10,27 @@ class Customer(BaseModel):
     first_name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     last_name = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
+    password = models.CharField(
+        max_length=255,
+        validators=[
+            RegexValidator(
+                regex=r"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})",
+                message="Password must have more than 8 characters, symbols, numbers, uppercase and lowercase",
+            )
+        ],
+    )
     phone_number = models.CharField(max_length=255)
-    points = models.IntegerField()
-    username = models.CharField(max_length=255)
+    points = models.IntegerField(default=0)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["email"], name="unique_email"),
+        ]
+
+    def save(self, *args, **kwargs):
+        self.email = self.email.lower()
+        self.first_name = self.first_name.capitalize()
+        self.last_name = self.last_name.capitalize()
+        self.phone_number = self.phone_number.replace(" ", "").replace("-", "").replace("+", "")
+        self.password = make_password(self.password)
+        return super().save(*args, **kwargs)

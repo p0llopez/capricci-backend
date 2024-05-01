1108ded42a571960b61c77ccc3851fc960f837e5
Pol Lopez <pol.lopez.cano@gmail.com>
2024-05-01 19:54:54 +0200

    feat: remove Review

--- src/product/use_cases/update_rating_and_reviews_number_use_case.py
+++ src/product/use_cases/update_rating_and_reviews_number_use_case.py
@@ -4,14 +4,13 @@ from django.db import transaction
 from django.db.models import Sum
 
 from src.product.models import Product
-from src.review.models import Review
 
 
 class UpdateRatingAndReviewsNumberUseCase:
     def __call__(self, product_id: UUID):
         product = Product.objects.get(id=product_id)
-        quantity_reviews = Review.objects.filter(product=product).count()
-        total_rating = Review.objects.filter(product=product).aggregate(Sum("rating"))["rating__sum"]
+        quantity_reviews = product.reviews.count()
+        total_rating = product.reviews.aggregate(Sum("rating"))["rating__sum"]
 
         with transaction.atomic():
             product.quantity_reviews = quantity_reviews

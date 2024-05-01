9b10f15878c0d4fa60dc3da36174655fe510e908
Pol Lopez <pol.lopez.cano@gmail.com>
2024-05-01 19:11:59 +0200

    feat: update ratings and reviews from new or deleted review

---
+++ src/product/use_cases/update_rating_and_reviews_number_use_case.py
@@ -0,0 +1,19 @@
+from uuid import UUID
+
+from django.db import transaction
+from django.db.models import Sum
+
+from src.product.models import Product
+from src.review.models import Review
+
+
+class UpdateRatingAndReviewsNumberUseCase:
+    def __call__(self, product_id: UUID):
+        product = Product.objects.get(id=product_id)
+        quantity_reviews = Review.objects.filter(product=product).count()
+        total_rating = Review.objects.filter(product=product).aggregate(Sum("rating"))["rating__sum"]
+
+        with transaction.atomic():
+            product.quantity_reviews = quantity_reviews
+            product.rating = total_rating / quantity_reviews
+            product.save()

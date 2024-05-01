197e4e484755f421614eaad3cd9723558abeee96
Pol Lopez <pol.lopez.cano@gmail.com>
2024-05-01 19:39:22 +0200

    feat: optimize with select related

--- src/review/admin.py
+++ src/review/admin.py
@@ -8,3 +8,6 @@ class ReviewAdmin(admin.ModelAdmin):
     list_display = ("id", "product", "rating", "comment", "customer")
     list_filter = ("product", "rating", "customer")
     search_fields = ("product", "rating", "comment", "customer")
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related("product", "customer")

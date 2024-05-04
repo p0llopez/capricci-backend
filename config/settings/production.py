1bbb9c1dd5d1410c57ebd4e7605d47010551a78b
Pol Lopez <pol.lopez.cano@gmail.com>
2024-05-04 14:40:54 +0200

    feat: s3 to Cloudflare R2

--- config/settings/production.py
+++ config/settings/production.py
@@ -18,20 +18,18 @@ DATABASES = {
 
 # aws settings
 # https://medium.com/@hrushi669/file-storage-with-aws-s3-buckets-upload-for-the-django-project-50ea7208c4b1
-AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
-AWS_DEFAULT_ACL = "public-read"
-AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
-AWS_LOCATION = "static"
+AWS_ACCESS_KEY_ID = env("R2_ACCESS_KEY")
+AWS_SECRET_ACCESS_KEY = env("R2_SECRET_KEY")
+AWS_STORAGE_BUCKET_NAME = env("R2_BUCKET_NAME")
+AWS_S3_ENDPOINT_URL = env("R2_ENDPOINT_URL")
+AWS_S3_SIGNATURE_VERSION = "s3v4"
 
 # Static files (CSS, JavaScript, images)
 STATIC_LOCATION = "static"
-STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/"
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATIC_URL = f"https://{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/{STATIC_LOCATION}/"
+STATICFILES_STORAGE = "config.storages.StaticStorage"
 
 # Media files
 MEDIA_LOCATION = "media"
-MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/"
+MEDIA_URL = f"https://{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/{MEDIA_LOCATION}/"
 DEFAULT_FILE_STORAGE = "config.storages.MediaStorage"

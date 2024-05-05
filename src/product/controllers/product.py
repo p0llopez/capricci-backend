b2108ea38437a9fce621833816698fc4caf7f45d
Pol Lopez <pol.lopez.cano@gmail.com>
2024-05-05 18:45:58 +0200

    feat: added serach filtering

--- src/product/controllers/product.py
+++ src/product/controllers/product.py
@@ -1,6 +1,7 @@
 from uuid import UUID
 
 from ninja_extra import api_controller, route
+from ninja_extra.searching import Searching, searching
 
 from src.product.models import Product
 from src.product.schemas import ProductSchema
@@ -10,6 +11,7 @@ from src.review.schemas import ReviewSchema
 @api_controller("/products")
 class ProductController:
     @route.get("", response=[(200, list[ProductSchema])], url_name="list")
+    @searching(Searching, search_fields=["@name", "@description"])
     def list_products(self):
         return 200, Product.objects.all()
 

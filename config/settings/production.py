3e45907233f7f64d0b0afc07f0a24371d1792b28
Pol Lopez <pol.lopez.cano@gmail.com>
2024-05-05 23:23:06 +0200

    feat: back to r2

--- config/settings/production.py
+++ config/settings/production.py
@@ -18,25 +18,19 @@ DATABASES = {
 
 # aws settings
 # https://medium.com/@hrushi669/file-storage-with-aws-s3-buckets-upload-for-the-django-project-50ea7208c4b1
-AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY")
-AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_KEY")
-AWS_STORAGE_BUCKET_NAME = env("AWS_BUCKET_NAME")
-AWS_S3_ENDPOINT_URL = env("AWS_ENDPOINT_URL")
-AWS_DEFAULT_ACL = "public-read"
-# AWS_S3_SIGNATURE_VERSION = "s3v4"
-# AWS_S3_CUSTOM_DOMAIN = "cdn.capriccipineda.es"
-
-AWS_S3_URL_PROTOCOL = "https"
-AWS_S3_USE_SSL = True
-AWS_S3_VERIFY = True
-
+AWS_ACCESS_KEY_ID = env("R2_ACCESS_KEY")
+AWS_SECRET_ACCESS_KEY = env("R2_SECRET_KEY")
+AWS_STORAGE_BUCKET_NAME = env("R2_BUCKET_NAME")
+AWS_S3_ENDPOINT_URL = env("R2_ENDPOINT_URL")
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+AWS_S3_CUSTOM_DOMAIN = "cdn.capriccipineda.es"
 
 # Static files (CSS, JavaScript, images)
 STATIC_LOCATION = "static"
-STATIC_URL = f"https://{AWS_S3_ENDPOINT_URL}/{STATIC_LOCATION}/"
+STATIC_URL = f"https://{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/{STATIC_LOCATION}/"
 STATICFILES_STORAGE = "config.storages.StaticStorage"
 
 # Media files
 MEDIA_LOCATION = "media"
-MEDIA_URL = f"https://{AWS_S3_ENDPOINT_URL}/{MEDIA_LOCATION}/"
+MEDIA_URL = f"https://{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/{MEDIA_LOCATION}/"
 DEFAULT_FILE_STORAGE = "config.storages.MediaStorage"
